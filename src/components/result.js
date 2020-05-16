a9c8466a627476211b1b2defca5b6256ddbd5599
Siosaia Fonua <sfonua10@churchofjesuschrist.org>
2020-05-16 09:59:34 -0600

    correct a link syntax

--- src/components/result.js
+++ src/components/result.js
@@ -4,12 +4,11 @@ import PropTypes from "prop-types"
 const Result = props => {
   return (
     <>
-    <a href="https://facebook.com/sharer/sharer.php?u=https://jovial-ritchie-46bbbe.netlify.app/"
-    <div className="result">
-      You scored <strong>{props.quizResult}</strong> out of 5!
-    </div>
+      <a href="https://facebook.com/sharer/sharer.php?u=https://jovial-ritchie-46bbbe.netlify.app/" />
+      <div className="result">
+        You scored <strong>{props.quizResult}</strong> out of 5!
+      </div>
     </>
-    
   )
 }
 
