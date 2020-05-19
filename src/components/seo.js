26a3b51f17a1c5d524b25dd060d5a444b9b9bb81
Siosaia Fonua <sfonua10@churchofjesuschrist.org>
2020-05-18 21:00:30 -0600

    test og tags on results page

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,8 +9,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-
-function SEO({ description, lang, meta, title }) {
+import favicon from "../images/favicon.ico"
+function SEO({ description, lang, meta, title, image = "", url = "" }) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -48,6 +48,14 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
         {
+          property: `og:image`,
+          content: image,
+        },
+        {
+          property: `og:url`,
+          content: url,
+        },
+        {
           property: `og:type`,
           content: `website`,
         },
@@ -68,6 +76,7 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
+      link={[{ rel: "shortcut icon", type: "image/png", href: `${favicon}` }]}
     />
   )
 }
