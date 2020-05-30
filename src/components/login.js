88e520a9f7e093c7d652147dea9e69387edbeaf0
Siosaia Fonua <sfonua10@churchofjesuschrist.org>
2020-05-29 20:01:38 -0600

    add new appId

--- src/components/login.js
+++ src/components/login.js
@@ -12,11 +12,7 @@ const Login = ({ onLogin }) => {
   }
   return(
     <FacebookLogin
-<<<<<<< HEAD
       appId="620898128511817"
-=======
-      appId="2941789695875677"
->>>>>>> 11e0d2cd5cba61cb6e9112dfaf0566a15c45b370
       autoLoad={false}
       fields="name,email,picture"
       onClick={onLogin}
